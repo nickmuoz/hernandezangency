2f220102bb072de5c3b42111aa7c5a8cfb1a6d62
unknown <nicolasmuoz@gmail.com>
2024-03-02 09:46:40 -0500

    tooglelenguage

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,58 +42,80 @@
     }
 
     // Function to toggle between English and Spanish text
-function toggleLanguage() {
-    console.log("toggleLanguage is called");
-    fetchJSONData(function(jsonData) {
-        var langButton = document.getElementById('languageButton').value; // Access value directly
-        var h1Element = document.querySelector('#titleHeader');
-        var pElement = document.querySelector('#textHeader');
-        var aElement = document.querySelector('#anchorHeader');
-        var textDescription = document.querySelector('#TextDescription');
-        var parraftDescription = document.querySelector('#parraftDescription');
-        var carouselTitle1 = document.querySelector('#carouselTitle1');
-        var descriptionTitle1 = document.querySelector('#descriptionTitle1');
-        var carouselTitle2 = document.querySelector('#carouselTitle2');
-        var descriptionTitle2 = document.querySelector('#descriptionTitle2');
-        var carouselTitle3 = document.querySelector('#carouselTitle3');
-        var descriptionTitle3 = document.querySelector('#descriptionTitle3');
-
-        //peding to make a services part 
-        
-        if (langButton === 'Español / Ingles') { // Check value directly
-            // Update button value
-            document.getElementById('languageButton').value = 'English / Español';
-            // Update content to English
-            h1Element.innerText = jsonData.intro.title.en.h1;
-            pElement.innerText = jsonData.intro.title.en.p;
-            aElement.innerText = jsonData.intro.title.en.a;
-            textDescription.innerText = jsonData.intro.description.en.h2;
-            parraftDescription.innerText = jsonData.intro.description.en.p;
-            carouselTitle1.innerText = jsonData.intro.description.en.carouselTitle1;
-            carouselTitle1.innerText = jsonData.intro.description.en.carouselTitle1;
-            descriptionTitle1.innerText = jsonData.intro.description.en.descriptionTitle1;
-            carouselTitle2.innerText = jsonData.intro.description.en.carouselTitle2;
-            descriptionTitle2.innerText = jsonData.intro.description.en.descriptionTitle2;
-            carouselTitle3.innerText = jsonData.intro.description.en.carouselTitle3;
-            descriptionTitle3.innerText = jsonData.intro.description.en.descriptionTitle3;
-        } else {
-            // Update button value
-            document.getElementById('languageButton').value = 'Español / Ingles';
-            // Update content to Spanish
-            h1Element.innerText = jsonData.intro.title.sp.h1;
-            pElement.innerText = jsonData.intro.title.sp.p;
-            aElement.innerText = jsonData.intro.title.sp.a;
-            textDescription.innerText = jsonData.intro.description.sp.h2;
-            parraftDescription.innerText = jsonData.intro.description.sp.p;
-            carouselTitle1.innerText = jsonData.intro.description.sp.carouselTitle1;
-            descriptionTitle1.innerText = jsonData.intro.description.sp.descriptionTitle1;
-            carouselTitle2.innerText = jsonData.intro.description.sp.carouselTitle2;
-            descriptionTitle2.innerText = jsonData.intro.description.sp.descriptionTitle3;
-            carouselTitle3.innerText = jsonData.intro.description.sp.carouselTitle3;
-            descriptionTitle3.innerText = jsonData.intro.description.sp.descriptionTitle3;
-        }
-    });
-}
+    function toggleLanguage() {
+        console.log("toggleLanguage is called");
+        fetchJSONData(function(jsonData) {
+            var langButton = document.getElementById('languageButton').value; // Access value directly
+            var h1Element = document.querySelector('#titleHeader');
+            var pElement = document.querySelector('#textHeader');
+            var aElement = document.querySelector('#anchorHeader');
+            var textDescription = document.querySelector('#TextDescription');
+            var parraftDescription = document.querySelector('#parraftDescription');
+            var carouselTitle1 = document.querySelector('#carouselTitle1');
+            var descriptionTitle1 = document.querySelector('#descriptionTitle1');
+            var carouselTitle2 = document.querySelector('#carouselTitle2');
+            var descriptionTitle2 = document.querySelector('#descriptionTitle2');
+            var carouselTitle3 = document.querySelector('#carouselTitle3');
+            var descriptionTitle3 = document.querySelector('#descriptionTitle3');
+            var titleServ = document.querySelector('#titleServ');
+            var descripServ = document.querySelector('#descripServ');
+            var serv1 = document.querySelector('#serv1');
+            var servText1 = document.querySelector('#servText1');
+            var serv2 = document.querySelector('#serv2');
+            var servText2 = document.querySelector('#servText2');
+            var serv3 = document.querySelector('#serv3');
+            var servText3 = document.querySelector('#servText3');
+            
+            if (langButton === 'Español / Ingles') {
+                // Update button value
+                document.getElementById('languageButton').value = 'English / Español';
+                // Update content to English
+                h1Element.innerText = jsonData.intro.title.en.h1;
+                pElement.innerText = jsonData.intro.title.en.p;
+                aElement.innerText = jsonData.intro.title.en.a;
+                textDescription.innerText = jsonData.intro.description.en.h2;
+                parraftDescription.innerText = jsonData.intro.description.en.p;
+                carouselTitle1.innerText = jsonData.intro.description.en.carouselTitle1;
+                descriptionTitle1.innerText = jsonData.intro.description.en.descriptionTitle1;
+                carouselTitle2.innerText = jsonData.intro.description.en.carouselTitle2;
+                descriptionTitle2.innerText = jsonData.intro.description.en.descriptionTitle2;
+                carouselTitle3.innerText = jsonData.intro.description.en.carouselTitle3;
+                descriptionTitle3.innerText = jsonData.intro.description.en.descriptionTitle3;
+                titleServ.innerText = jsonData.intro.services.en.titleServ;
+                descripServ.innerText = jsonData.intro.services.en.descripServ;
+                serv1.innerText = jsonData.intro.services.en.serv1;
+                servText1.innerText = jsonData.intro.services.en.servText1;
+                serv2.innerText = jsonData.intro.services.en.serv2;
+                servText2.innerText = jsonData.intro.services.en.servText2;
+                serv3.innerText = jsonData.intro.services.en.serv3;
+                servText3.innerText = jsonData.intro.services.en.servText3;
+            } else {
+                // Update button value
+                document.getElementById('languageButton').value = 'Español / Ingles';
+                // Update content to Spanish
+                h1Element.innerText = jsonData.intro.title.sp.h1;
+                pElement.innerText = jsonData.intro.title.sp.p;
+                aElement.innerText = jsonData.intro.title.sp.a;
+                textDescription.innerText = jsonData.intro.description.sp.h2;
+                parraftDescription.innerText = jsonData.intro.description.sp.p;
+                carouselTitle1.innerText = jsonData.intro.description.sp.carouselTitle1;
+                descriptionTitle1.innerText = jsonData.intro.description.sp.descriptionTitle1;
+                carouselTitle2.innerText = jsonData.intro.description.sp.carouselTitle2;
+                descriptionTitle2.innerText = jsonData.intro.description.sp.descriptionTitle2;
+                carouselTitle3.innerText = jsonData.intro.description.sp.carouselTitle3;
+                descriptionTitle3.innerText = jsonData.intro.description.sp.descriptionTitle3;
+                titleServ.innerText = jsonData.intro.services.sp.titleServ;
+                descripServ.innerText = jsonData.intro.services.sp.descripServ;
+                serv1.innerText = jsonData.intro.services.sp.serv1;
+                servText1.innerText = jsonData.intro.services.sp.servText1;
+                serv2.innerText = jsonData.intro.services.sp.serv2;
+                servText2.innerText = jsonData.intro.services.sp.servText2;
+                serv3.innerText = jsonData.intro.services.sp.serv3;
+                servText3.innerText = jsonData.intro.services.sp.servText3;
+            }
+        });
+    }
+    
 
 	
 
