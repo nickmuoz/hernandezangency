170fc7af28b444bde855f3c77cfa5fdf2120a31a
unknown <nicolasmuoz@gmail.com>
2024-03-06 21:11:58 -0500

    carousel part remodelation img complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,6 @@
     xxsmall: [null, "320px"],
   });
 
-  function testPrint() {
-    console.log("este es el archivo main");
-  }
   // Play initial animations on page load.
   $window.on("load", function () {
     window.setTimeout(function () {
@@ -43,140 +40,134 @@
 
   // Function to toggle between English and Spanish text
   function toggleLanguage() {
-    console.log("toggleLanguage is called");
     fetchJSONData(function (jsonData) {
-      var langButton = document.getElementById("languageButton").value; // Access value directly
-      var h1Element = document.querySelector("#titleHeader");
-      var pElement = document.querySelector("#textHeader");
-      var aElement = document.querySelector("#anchorHeader");
-      var textDescription = document.querySelector("#TextDescription");
-      var parraftDescription = document.querySelector("#parraftDescription");
-      var carouselTitle1 = document.querySelector("#carouselTitle1");
-      var descriptionTitle1 = document.querySelector("#descriptionTitle1");
-      var carouselTitle2 = document.querySelector("#carouselTitle2");
-      var descriptionTitle2 = document.querySelector("#descriptionTitle2");
-      var carouselTitle3 = document.querySelector("#carouselTitle3");
-      var descriptionTitle3 = document.querySelector("#descriptionTitle3");
-      var titleServ = document.querySelector("#titleServ");
-      var descripServ = document.querySelector("#descripServ");
-      var serv1 = document.querySelector("#serv1");
-      var servText1 = document.querySelector("#servText1");
-      var serv2 = document.querySelector("#serv2");
-      var servText2 = document.querySelector("#servText2");
-      var serv3 = document.querySelector("#serv3");
-      var servText3 = document.querySelector("#servText3");
-      var button1 = document.querySelector("#button1");
-      var button2 = document.querySelector("#button2");
-      var button3 = document.querySelector("#button3");
-      var titleContact = document.querySelector("#titleContact");
-      var contacttext1 = document.querySelector("#contacttext1");
-      var contacttext2 = document.querySelector("#contacttext2");
-      var contacttext3 = document.querySelector("#contacttext3");
-      var fullNameContac = document.querySelector("#fullNameContac");
-      var phonecontac = document.querySelector("#phonecontac");
-      var emailContac = document.querySelector("#emailContac");
-      var opcionText = document.querySelector("#opcionText");
-      var opcionText1 = document.querySelector("#opcionText1");
-      var opcionText2 = document.querySelector("#opcionText2");
-      var opcionText3 = document.querySelector("#opcionText3");
-      var messageContac = document.querySelector("#messageContac");
-      var buttonContac = document.querySelector("#buttonContac");
+      var langButton = document.getElementById("languageButton").value;
+      var language = langButton === "Español / Ingles" ? "en" : "sp";
+      var languageData = jsonData.intro.title[language];
 
-      if (langButton === "Español / Ingles") {
-        // Update button value
-        document.getElementById("languageButton").value = "English / Español";
-        // Update content to English
-        h1Element.innerText = jsonData.intro.title.en.h1;
-        pElement.innerText = jsonData.intro.title.en.p;
-        aElement.innerText = jsonData.intro.title.en.a;
-        textDescription.innerText = jsonData.intro.description.en.h2;
-        parraftDescription.innerText = jsonData.intro.description.en.p;
-        carouselTitle1.innerText = jsonData.intro.description.en.carouselTitle1;
-        descriptionTitle1.innerText =
-          jsonData.intro.description.en.descriptionTitle1;
-        carouselTitle2.innerText = jsonData.intro.description.en.carouselTitle2;
-        descriptionTitle2.innerText =
-          jsonData.intro.description.en.descriptionTitle2;
-        carouselTitle3.innerText = jsonData.intro.description.en.carouselTitle3;
-        descriptionTitle3.innerText =
-          jsonData.intro.description.en.descriptionTitle3;
-        titleServ.innerText = jsonData.intro.services.en.titleServ;
-        descripServ.innerText = jsonData.intro.services.en.descripServ;
-        serv1.innerText = jsonData.intro.services.en.serv1;
-        servText1.innerText = jsonData.intro.services.en.servText1;
-        serv2.innerText = jsonData.intro.services.en.serv2;
-        servText2.innerText = jsonData.intro.services.en.servText2;
-        serv3.innerText = jsonData.intro.services.en.serv3;
-        servText3.innerText = jsonData.intro.services.en.servText3;
-        button1.innerText = jsonData.intro.services.en.buttonText;
-        button2.innerText = jsonData.intro.services.en.buttonText;
-        button3.innerText = jsonData.intro.services.en.buttonText;
-        titleContact.innerText = jsonData.intro.contact.en.titleContact;
-        contacttext1.innerText = jsonData.intro.contact.en.contacttext1;
-        contacttext2.innerText = jsonData.intro.contact.en.contacttext2;
-        contacttext3.innerText = jsonData.intro.contact.en.contacttext3;
-        fullNameContac.placeholder = jsonData.intro.contact.en.fullNameContac;
-        phonecontac.placeholder = jsonData.intro.contact.en.phonecontac;
-        emailContac.placeholder = jsonData.intro.contact.en.emailContac;
-        opcionText.innerText = jsonData.intro.contact.en.opcionText;
-        opcionText1.innerText = jsonData.intro.contact.en.opcionText1;
-        opcionText2.innerText = jsonData.intro.contact.en.opcionText2;
-        opcionText3.innerText = jsonData.intro.contact.en.opcionText3;
-        messageContac.placeholder = jsonData.intro.contact.en.messageContac;
-        buttonContac.innerText = jsonData.intro.contact.en.buttonContac;
-      } else {
-        // Update button value
-        document.getElementById("languageButton").value = "Español / Ingles";
-        // Update content to Spanish
-        h1Element.innerText = jsonData.intro.title.sp.h1;
-        pElement.innerText = jsonData.intro.title.sp.p;
-        aElement.innerText = jsonData.intro.title.sp.a;
-        textDescription.innerText = jsonData.intro.description.sp.h2;
-        parraftDescription.innerText = jsonData.intro.description.sp.p;
-        carouselTitle1.innerText = jsonData.intro.description.sp.carouselTitle1;
-        descriptionTitle1.innerText =
-          jsonData.intro.description.sp.descriptionTitle1;
-        carouselTitle2.innerText = jsonData.intro.description.sp.carouselTitle2;
-        descriptionTitle2.innerText =
-          jsonData.intro.description.sp.descriptionTitle2;
-        carouselTitle3.innerText = jsonData.intro.description.sp.carouselTitle3;
-        descriptionTitle3.innerText =
-          jsonData.intro.description.sp.descriptionTitle3;
-        titleServ.innerText = jsonData.intro.services.sp.titleServ;
-        descripServ.innerText = jsonData.intro.services.sp.descripServ;
-        serv1.innerText = jsonData.intro.services.sp.serv1;
-        servText1.innerText = jsonData.intro.services.sp.servText1;
-        serv2.innerText = jsonData.intro.services.sp.serv2;
-        servText2.innerText = jsonData.intro.services.sp.servText2;
-        serv3.innerText = jsonData.intro.services.sp.serv3;
-        servText3.innerText = jsonData.intro.services.sp.servText3;
-        button1.innerText = jsonData.intro.services.sp.buttonText;
-        button2.innerText = jsonData.intro.services.sp.buttonText;
-        button3.innerText = jsonData.intro.services.sp.buttonText;
-        titleContact.innerText = jsonData.intro.contact.sp.titleContact;
-        contacttext1.innerText = jsonData.intro.contact.sp.contacttext1;
-        contacttext2.innerText = jsonData.intro.contact.sp.contacttext2;
-        contacttext3.innerText = jsonData.intro.contact.sp.contacttext3;
-        fullNameContac.placeholder = jsonData.intro.contact.sp.fullNameContac;
-        phonecontac.placeholder = jsonData.intro.contact.sp.phonecontac;
-        emailContac.placeholder = jsonData.intro.contact.sp.emailContac;
-        opcionText.innerText = jsonData.intro.contact.sp.opcionText;
-        opcionText1.innerText = jsonData.intro.contact.sp.opcionText1;
-        opcionText2.innerText = jsonData.intro.contact.sp.opcionText2;
-        opcionText3.innerText = jsonData.intro.contact.sp.opcionText3;
-        messageContac.placeholder = jsonData.intro.contact.sp.messageContac;
-        buttonContac.innerText = jsonData.intro.contact.sp.buttonContac;
-      }
+      // Update button value
+      document.getElementById("languageButton").value =
+        language === "en" ? "English / Español" : "Español / Ingles";
+
+      // Update content
+      document.querySelector("#titleHeader").innerText = languageData.h1;
+      document.querySelector("#textHeader").innerText = languageData.p;
+      document.querySelector("#anchorHeader").innerText = languageData.a;
+      document.querySelector("#TextDescription").innerText =
+        jsonData.intro.description[language].h2;
+      document.querySelector("#parraftDescription").innerText =
+        jsonData.intro.description[language].p;
+      document.querySelector("#carouselTitle1").innerText =
+        jsonData.intro.description[language].carouselTitle1;
+      document.querySelector("#descriptionTitle1").innerText =
+        jsonData.intro.description[language].descriptionTitle1;
+      document.querySelector("#carouselTitle2").innerText =
+        jsonData.intro.description[language].carouselTitle2;
+      document.querySelector("#descriptionTitle2").innerText =
+        jsonData.intro.description[language].descriptionTitle2;
+      document.querySelector("#carouselTitle3").innerText =
+        jsonData.intro.description[language].carouselTitle3;
+      document.querySelector("#descriptionTitle3").innerText =
+        jsonData.intro.description[language].descriptionTitle3;
+      document.querySelector("#titleServ").innerText =
+        jsonData.intro.services[language].titleServ;
+      document.querySelector("#descripServ").innerText =
+        jsonData.intro.services[language].descripServ;
+      document.querySelector("#serv1").innerText =
+        jsonData.intro.services[language].serv1;
+      document.querySelector("#servText1").innerText =
+        jsonData.intro.services[language].servText1;
+      document.querySelector("#serv2").innerText =
+        jsonData.intro.services[language].serv2;
+      document.querySelector("#servText2").innerText =
+        jsonData.intro.services[language].servText2;
+      document.querySelector("#serv3").innerText =
+        jsonData.intro.services[language].serv3;
+      document.querySelector("#servText3").innerText =
+        jsonData.intro.services[language].servText3;
+      document.querySelector("#button1").innerText =
+        jsonData.intro.services[language].buttonText;
+      document.querySelector("#button2").innerText =
+        jsonData.intro.services[language].buttonText;
+      document.querySelector("#button3").innerText =
+        jsonData.intro.services[language].buttonText;
+      document.querySelector("#titleContact").innerText =
+        jsonData.intro.contact[language].titleContact;
+      document.querySelector("#contacttext1").innerText =
+        jsonData.intro.contact[language].contacttext1;
+      document.querySelector("#contacttext2").innerText =
+        jsonData.intro.contact[language].contacttext2;
+      document.querySelector("#contacttext3").innerText =
+        jsonData.intro.contact[language].contacttext3;
+      document.querySelector("#fullNameContac").placeholder =
+        jsonData.intro.contact[language].fullNameContac;
+      document.querySelector("#phonecontac").placeholder =
+        jsonData.intro.contact[language].phonecontac;
+      document.querySelector("#emailContac").placeholder =
+        jsonData.intro.contact[language].emailContac;
+      document.querySelector("#opcionText").innerText =
+        jsonData.intro.contact[language].opcionText;
+      document.querySelector("#opcionText1").innerText =
+        jsonData.intro.contact[language].opcionText1;
+      document.querySelector("#opcionText2").innerText =
+        jsonData.intro.contact[language].opcionText2;
+      document.querySelector("#opcionText3").innerText =
+        jsonData.intro.contact[language].opcionText3;
+      document.querySelector("#messageContac").placeholder =
+        jsonData.intro.contact[language].messageContac;
+      document.querySelector("#buttonContac").innerText =
+        jsonData.intro.contact[language].buttonContac;
     });
   }
 
+  //function to select modal Theme
+  function selectTheme(theme) {
+    fetchJSONData(function (jsonData) {
+        var modalTitle = jsonData.intro.modal[theme][getCurrentLanguage()].titleServ1;
+        var modalSubTitle = jsonData.intro.modal[theme][getCurrentLanguage()].subTitle;
+        var modalImgSrc1 = jsonData.intro.modal[theme].img1Modal;
+        var modalImgSrc2 = jsonData.intro.modal[theme].img2Modal;
+        var modalImgSrc3 = jsonData.intro.modal[theme].img3Modal;
+        var modalImgSrc4 = jsonData.intro.modal[theme].img4Modal;
+
+        // Update the modal elements
+        var modalTitleElement = document.querySelector("#exampleModalLabel");
+        var modalSubTitleElement = document.querySelector("#subTitle");
+        var modalImgElement1 = document.querySelector("#imgModal1");
+        var modalImgElement2 = document.querySelector("#imgModal2");
+        var modalImgElement3 = document.querySelector("#imgModal3");
+        var modalImgElement4 = document.querySelector("#imgModal4");
+
+        modalTitleElement.innerText = modalTitle;
+        modalSubTitleElement.innerText = modalSubTitle;
+        modalImgElement1.src = modalImgSrc1;
+        modalImgElement2.src = modalImgSrc2;
+        modalImgElement3.src = modalImgSrc3;
+        modalImgElement4.src = modalImgSrc4;
+    });
+}
+
+
   // Attach click event to language button
   $(document).ready(function () {
-    // Adjuntar controlador de eventos al botón de cambio de idioma
+    // Event controller
     $("#languageButton").click(function () {
       toggleLanguage();
     });
+    $("#button1").click(function () {
+      selectTheme("Remodelin");
+    });
+    $("#button2").click(function () {
+      selectTheme("Accounting");
+    });
+    $("#button3").click(function () {
+      selectTheme("Longevidad");
+    });
   });
 
-  // Resto de tu código sigue igual
+  // Helper function to get the current language
+  function getCurrentLanguage() {
+    var languageSet = document.getElementById("languageButton").value;
+    return languageSet === "Español / Ingles" ? "sp" : "en";
+  }
 })(jQuery);
